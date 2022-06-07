06c45fc4d50845c4f73bb868bc4a5a36ec44185c
AnonymousXC <cvxanonymous007@gmail.com>
2022-06-07 17:51:37 +0530

    Now can Bold, Italic && Both. Made Faster

--- MDEditor.js
+++ MDEditor.js
@@ -12,13 +12,29 @@ const MDEditor = function(options) {
         let insert = false;
 
         if(html.search(/(\*(\w+)\*)/g)) {
+            if(html.search(/(\*\*\*(\w+)\*\*\*)/g)) {
+                insert = true;
+                html = html.replace(/(\*\*\*(\w+)\*\*\*)/g , "<strong><i>$2</i></strong>");
+            }
+            if(html.search(/(\*\*(\w+)\*\*)/g)) {
+                insert = true;
+                html = html.replace(/(\*\*(\w+)\*\*)/g , "<strong>$2</strong>");
+            }
             insert = true;
             html = html.replace(/(\*(\w+)\*)/g , "<i>$2</i>");
         }
 
-        if(html.search(/(\*\*(\w+)\*\*)/g)) {
+        if(html.search(/(\_(\w+)\_)/g)) {
+            if(html.search(/(\_\_\_(\w+)\_\_\_)/g)) {
+                insert = true;
+                html = html.replace(/(\_\_\_(\w+)\_\_\_)/g , "<strong><i>$2</i></strong>");
+            }
+            if(html.search(/(\_\_(\w+)\_\_)/g)) {
+                insert = true;
+                html = html.replace(/(\_\_(\w+)\_\_)/g , "<strong>$2</strong>");
+            }
             insert = true;
-            html = html.replace(/(\*\*(\w+)\*\*)/g , "<strong>$2</strong>");
+            html = html.replace(/(\_(\w+)\_)/g , "<i>$2</i>");
         }
 
 
